ee4f1eb07be5c55d3b5320dfcab21bd1ea421c61
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-11-18 13:30:59 -0300

    ✨ Raw Text Extraction

--- src/main.py
+++ src/main.py
@@ -12,8 +12,25 @@ def get_exam(exam_path: str) -> PdfReader:
     return PdfReader(exam_path)
 
 
+def get_raw_text(exam: PdfReader) -> [str]:
+    """Obtém texto bruto do arquivo pdf da prova, separado por página
+
+    Args:
+        exam (PdfReader): prova
+
+    Returns:
+        [str]: textos brutos de de cada página (lista)
+    """
+    pages_text = [page.extract_text() for page in exam.pages]
+    
+    return pages_text
+
+
 def main():
     print("MELVIN EXAM SCRAPPER")
+    
+    exam = get_exam("../exams/enem.pdf")
+    raw_txt = get_raw_text(exam)
 
 
 if __name__ == "__main__":
