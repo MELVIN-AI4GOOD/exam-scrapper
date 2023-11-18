32d82b2a7847c8a7667d94296c644051d433e654
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-11-18 14:07:58 -0300

    ✨ save the raw text in txt

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from pypdf import PdfReader
 
+DEFAULT_RAW_TXT_PATH = '../raw_txts'
+
 def get_exam(exam_path: str) -> PdfReader:
     """Obtém o pdf da prova e converte-o em uma classe PdfReader
 
@@ -12,18 +14,36 @@ def get_exam(exam_path: str) -> PdfReader:
     return PdfReader(exam_path)
 
 
-def get_raw_text(exam: PdfReader) -> [str]:
-    """Obtém texto bruto do arquivo pdf da prova, separado por página
+def get_raw_text(exam: PdfReader) -> str:
+    """Obtém texto bruto do arquivo pdf da prova
 
     Args:
         exam (PdfReader): prova
 
     Returns:
-        [str]: textos brutos de de cada página (lista)
+        str: texto bruto da prova inteira
     """
     pages_text = [page.extract_text() for page in exam.pages]
+    raw_txt = "".join(pages_text)
     
-    return pages_text
+    return raw_txt
+
+
+def save_raw_text(exam_name: str, text: str) -> None:
+    """Salva o texto bruto da prova em txt, na pasta /raw_txts
+
+    Args:
+        exam_name (str): nome da prova
+        text (str): texto bruto do exame
+    """
+    file_extension = ".txt"
+    full_path = DEFAULT_RAW_TXT_PATH + '/' + exam_name + file_extension
+    
+    f = open(full_path, "w")
+    f.write(text)
+    f.close()
+    
+    return
 
 
 def main():
@@ -31,6 +51,7 @@ def main():
     
     exam = get_exam("../exams/enem.pdf")
     raw_txt = get_raw_text(exam)
+    save_raw_text("texto_bruto_enem", raw_txt)
 
 
 if __name__ == "__main__":
